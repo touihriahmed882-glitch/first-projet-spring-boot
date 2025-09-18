28dce251a58141b96137e31d82b4e3331d7e183d
ghareeb <touihriahmed@gmail.com>
2025-09-18 13:22:47 +0100

    le back-end spring boot

---
+++ Documents/workspace-sts/Utilisateur1/src/main/java/com/example/Utilisatuer1/entity/User.java
@@ -0,0 +1,88 @@
+package com.example.Utilisatuer1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String role;
+    private String mail;
+    private String password;
+    private String adresse;
+
+    @Column(name = "num_telephone")
+    private String numTelephone;
+
+    // ===== Getters et Setters =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getNumTelephone() {
+        return numTelephone;
+    }
+
+    public void setNumTelephone(String numTelephone) {
+        this.numTelephone = numTelephone;
+    }
+}
---
+++ Documents/workspace-sts/Utilisateur1/src/test/java/com/example/Utilisatuer1/Utilisateur1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Utilisatuer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Utilisateur1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
